0ec0b3c66f99dfde72137156f5fc990e0d19054a
EhaLotafe <overcomeeha@gmail.com>
2026-01-09 09:12:33 +0200

    Fix responsive issues and global overflow

--- index.tsx
+++ index.tsx
@@ -2,6 +2,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import './index.css';
 
 // On récupère l'élément racine défini dans index.html
 const rootElement = document.getElementById('root');
